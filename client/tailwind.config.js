dd3fba4c12d57da7ce8e480d7132ee8f447392c3
Akif <akifshah7@gmail.com>
2024-06-30 03:02:56 +0530

    fixed iconify icons not rendering issue

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,3 +1,5 @@
+const { addDynamicIconSelectors } = require('@iconify/tailwind');
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -6,10 +8,11 @@ export default {
       colors: {
         "light-gray": "#f0f2f9",
         "dark-gray": "#656e83",
+        "black": "#4a5162",
         "t-light-gray": "#828ba2",
-        "t-blue": "#1b91f0"
+        "t-blue": "#1b91f0",
       }
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), addDynamicIconSelectors()],
 };
