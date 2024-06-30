34e1f407c5307676637dd2a5305e024b06d15f17
Akif <akifshah7@gmail.com>
2024-06-30 21:34:51 +0530

    common input field

---
+++ client/src/common/components/Input/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+type InputProps = {
+  labelText?: string;  
+  placeholder?: string;
+  customStyles?: string;
+};
+
+const Input: React.FC<InputProps> = ({ placeholder, labelText, customStyles }) => {
+  return (
+    <div className="w-[45%] mx-4">
+      <label className="mb-2 text-t-light-gray text-xs">{labelText || ""}</label>
+      <input
+        placeholder={placeholder || ""}
+        className={`bg-light-gray rounded-sm p-4 w-full focus:outline-none border-b-0 focus:border-b-2 focus:border-blue-400 ${customStyles}`}
+      />
+    </div>
+  );
+};
+
+export default Input;
